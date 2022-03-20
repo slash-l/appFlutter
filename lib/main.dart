f41b6a6b3512c690aa5668158cf8b88877c488cf
slash-l <slash_mumu@163.com>
2022-03-20 20:14:07 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello JFrog'),
+        ),
+        body: const Center(
+          child: Text('This is the first Flutter App'),
+        ),
+      ),
+    );
+  }
+}
